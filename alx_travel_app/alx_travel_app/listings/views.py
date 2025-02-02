1dce5d3bc62e4942a483c3068eacee7658b83f4b
sange5 <163669819+sange5@users.noreply.github.com>
2025-02-02 20:14:07 +0300

    Add files via upload

---
+++ alx_travel_app/alx_travel_app/listings/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+from .tasks import send_booking_confirmation_email
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        booking = serializer.save()
+        send_booking_confirmation_email.delay(booking.id)
